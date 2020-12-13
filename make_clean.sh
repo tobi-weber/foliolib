3b96285363c3ffbbe2ce27011b63abed56f43458
Tobias Weber <tobi-weber@gmx.de>
2020-12-13 19:15:53 +0100

    some work

---
+++ make_clean.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+python3 setup.py clean
+debian/rules clean
+echo "Remove build MANIFEST dist *.egg-info *.pyc *.pyo __pycache__"
+rm -rf build/ MANIFEST dist/ *.egg-info
+find . -name '*.pyc' -delete
+find . -name '*.pyo' -delete
+find . -name '__pycache__' -delete
+cd docs
+make clean
+echo "Remove generated docs"
+rm -f source/generated/*
+cd ..
