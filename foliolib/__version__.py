ef1982f449968f2eb2d31026ac42fc21c070cdcb
Tobias Weber <tobi-weber@gmx.de>
2023-05-07 11:07:45 +0200

    release 0.3.4a1

--- foliolib/__version__.py
+++ foliolib/__version__.py
@@ -4,7 +4,7 @@
 __title__ = "foliolib"
 __description__ = "Okapi/Folio Manager and Library"
 __url__ = "https://github.com/tobi-weber/foliolib"
-__version__ = "0.3.3a2"
+__version__ = "0.3.4a1"
 __author__ = "Tobias Weber"
 __author_email__ = "tobi-weber@gmx.de"
 __license__ = "GNU General Public License v3 (GPLv3)"
